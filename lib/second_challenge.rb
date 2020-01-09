299b2be76f205397d236d818b645028f623b4712
Bri Turner <blturner3527@gmail.com>
2020-01-09 20:18:02 +0000

    Automatically backed up by Learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -10,9 +10,7 @@ def second_challenge
       hero: {
         
       },
-      hero_friends: {
-        
-      }
+      hero_friends: []
     }, 
     :capulet => {
       patriarch: {
@@ -24,7 +22,7 @@ def second_challenge
       heroine: {
         
       },
-      heroine_friends: []]
+      heroine_friends: []
     }}
 
   
