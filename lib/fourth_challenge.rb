e3e5aad142e9dbb732aab1415432132538ee546b
Bri Turner <blturner3527@gmail.com>
2020-01-09 20:12:33 +0000

    Automatically backed up by Learn

--- lib/fourth_challenge.rb
+++ lib/fourth_challenge.rb
@@ -13,7 +13,7 @@ def fourth_challenge
         {
           name: 'Mercutio',
           age: '18',
-          attitude: 'worried'
+          attitude: 'hot-headed'
         }
         }
       ]
@@ -23,7 +23,16 @@ def fourth_challenge
       :matriarch => {name: "Lady Capulet", age: "51"},
       :heroine => {name: "Juliet", age: "15", status: "alive"},
       :heroine_friends => [
-        #your hashes here!
+        {
+          name: 'Steven',
+          age: '30',
+          attitude: 'confused'
+        }
+        {
+          name: 'Nurse',
+          age: '44',
+          attitude: 'worried'
+        }
       ]
    }
 }
